171b4c15018cea5b94c948b34df69519ec889d6e
Bagas Febrian <bagas@example.com>
2026-06-09 13:40:48 +0800

    initial commit

---
+++ resources/views/pages/category/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h1 class="text-2x1 font-bold text-blue-500">Ngeri Bosque</h1>
+    {{-- Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead --}}
+</div>
