cb04c8c673e897661111a3b0eaaa50c7664f838b
Bagas Febrian <bagas@example.com>
2026-06-09 14:53:05 +0800

    commit

--- resources/views/pages/category/⚡index.blade.php
+++ resources/views/pages/category/⚡index.blade.php
@@ -9,8 +9,10 @@ new class extends Component
 ?>
 
 <div>
-    <h1 class="text-2x1 font-bold text-blue-500">Belajar Membuat Category</h1>
-    <h2>1.1 Memahami Category</h2>
-    <p>Category adalah sebuah fitur yang digunakan untuk mengelompokkan data berdasarkan kategori tertentu
-    {{-- Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead --}}
+<div class="flex items-center">
+  <img src="path/to/image.jpg" />
+  <div>
+    <strong>Bagas Ferbian</strong>
+    <span>Vice Admiral</span>
+  </div>
 </div>
